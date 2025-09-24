c0a9426fbebcf91b1b22e817d03eaf20338c950c
B.Nandhakumar <nandhakumarid023@gmail.com>
2025-09-24 15:36:23 +0530

    settings.py

--- bluezqr/bluezqr/settings.py
+++ bluezqr/bluezqr/settings.py
@@ -17,7 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 if not SECRET_KEY:
     raise ValueError("SECRET_KEY environment variable not set!")
 
